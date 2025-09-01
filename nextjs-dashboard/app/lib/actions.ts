4c369aa040836b240ba32da31243cd9fdde453b4
PARTH DUDHE <parthdudhe06@gmail.com>
2025-09-01 22:46:57 +0530

    fest: added CRUD functions

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,7 +1,10 @@
 'use server';
   
 import { z } from 'zod';
- 
+import postgres from 'postgres';
+ import { revalidatePath } from 'next/cache';
+ import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -9,7 +12,11 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
- 
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
@@ -19,7 +26,36 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   });
    const amountInCents = amount * 100;
-
+    const date = new Date().toISOString().split('T')[0];
+     await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
   // Test it out:
   //console.log(rawFormData);
+   
+  revalidatePath('/dashboard/invoices');
+   redirect('/dashboard/invoices');
+}
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
